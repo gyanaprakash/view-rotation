dcdc055fb0be788b2ce5fe6428d43f7c49ba25ea
Bsetecip10 <bsetecip10@Nirmals-Mac-mini.local>
2014-12-29 17:07:42 +0530

    Initial Commit

---
+++ Rotation/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Rotation
+//
+//  Created by Bsetecip10 on 29/12/14.
+//  Copyright (c) 2014 gyana. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
